306f4a2b2da2b783a6a89ace37d39090eabd2fab
Your Name <you@example.com>
2017-02-03 15:06:38 -0500

    make dashboard work on heroku

--- index.js
+++ index.js
@@ -31,10 +31,15 @@ var ParseDashboard = require('parse-dashboard');
 var dashboard = new ParseDashboard({
   "apps": [
     {
+      appName:  process.env.APP_ID || 'myAppId',
+      databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+      cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
       appId: process.env.APP_ID || 'myAppId',
       masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
       serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-      appName:  process.env.APP_ID || 'myAppId',
+      liveQuery: {
+        classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+      }
     }
   ],
   "users": [
